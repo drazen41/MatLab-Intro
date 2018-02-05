b14d9c35dcbd30f6437a8fabe4c55169f71e47af
drazen41 <drazen.svec@gmail.com>
2018-02-05 09:24:53 +0100

    accelerate

--- hw3/accelerate.m
+++ hw3/accelerate.m
@@ -1,8 +1,5 @@
 function amag = accelerate(F1,F2,m)
-  f1 = sqrt(F1(1)*F1(1)+F1(2)*F1(2)+F1(3)*F1(3));
-  f2 = sqrt(F2(1)*F2(1)+F2(2)*F2(2)+F2(3)*F2(3));
-  fSum = sqrt(f1*f1 + f2*f2);
-  amag = fSum / m;
-  %F = F1*F1+F2*f2;
-  % amag = F./m;
+  F = F1 + F2;
+  amag = sqrt(F(1)*F(1)+F(2)*F(2)+F(3)*F(3)) ./ m;
+  
   

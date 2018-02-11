9afe84f8ab46352c86fcd599ccd652814354f578
drazen <drazen.svec@outlook.com>
2018-02-11 20:20:48 +0100

    hw6 - spiral pendulum

--- hw6/pendulum.m
+++ hw6/pendulum.m
@@ -1,2 +1,16 @@
 function T=pendulum(L,a0)
-  
+  T = 0;
+    if L > 0
+        dt = 1e-6;
+        g = 9.8;
+        angle = abs(angle0);
+        omega = 0;
+        T = 0;
+        while angle > 0
+           a = g*sin(angle)/L;
+           omega = omega + dt * a;
+           angle = angle - dt * omega;
+           T = T + dt;
+        end
+        T = T * 4;
+end
--- hw6/spiral_diag_sum.m
+++ hw6/spiral_diag_sum.m
@@ -1,2 +1,3 @@
 function s=spiral_diag_sum(n)
-  
+   v = 3:2:n;
+s = 1 + sum(4*v.^2 - 6*(v-1)); 
